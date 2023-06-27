6693e0f8c46b09b7ee80409b817145014a07c3d0
MOJ_SDP <jstzxhl@yeah.net>
2023-06-27 18:46:43 +0800

    Add files via upload

--- scripts/datautil/decpr_re.m
+++ scripts/datautil/decpr_re.m
@@ -1,9 +1,9 @@
-function [y] = decF1(data)
+function [y] = decpr_re(data)
 
 A= confusionmat(data(:,1),data(:,3));
-   A = A';
-   precision = diag(A)./(sum(A,2) + 0.0001);  
-   recall = diag(A)./(sum(A,1)+0.0001)'; 
-   precision = mean(precision)
-   recall = mean(recall)
-y=pricision,recall
+	A = A';
+	precision = diag(A)./(sum(A,2) + 0.0001);	
+	recall = diag(A)./(sum(A,1)+0.0001)'; 
+	precision = mean(precision)
+	recall = mean(recall)
+y=precision,recall
